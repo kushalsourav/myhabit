86573665f6985a979e9c53ed96e4719d21783874
kushalsourav <kushalsourav2@gmail.com>
2022-12-23 15:20:59 +0530

    added modal and sidebar

--- src/router/Router/Router.jsx
+++ src/router/Router/Router.jsx
@@ -6,6 +6,7 @@ import Profile from "../../pages/Profile/Profile";
 import Habit from "../../pages/Habit/Habit";
 import WelcomePage from "../../pages/WelcomePage/WelcomePage"
 import PageNotFound from "../PageNotFound/PageNotFound";
+import RequireAuth from '../../contexts/AuthContext/RequireAuth/RequireAuth';
 
 
 
@@ -13,11 +14,11 @@ const Router = () => {
   return (
     <Routes>
         <Route path="/" element={<WelcomePage />} />
-        <Route path="/Home" element={<Home />} />
-        <Route path="/Label" element={<Label /> }/>
-        <Route path="/Habit" element={<Habit />} />
-        <Route path="/Archive" element={<Archives />} />
-        <Route path="/Profile" element={<Profile />} />
+        <Route path="/Home" element={<RequireAuth><Home /></RequireAuth>} />
+        <Route path="/Label" element={<RequireAuth><Label /> </RequireAuth>}/>
+        <Route path="/Habit" element={<RequireAuth><Habit /></RequireAuth>} />
+        <Route path="/Archive" element={<RequireAuth><Archives /></RequireAuth> } />
+        <Route path="/Profile" element={<RequireAuth><Profile /></RequireAuth>} />
         <Route path="*" element={<PageNotFound/>} />
     </Routes>
   )
